169fe8ba8bff905ff751f0e896f6ff0611f423af
Mark Penrod <mark.penrod@pomona.edu>
2018-03-26 20:46:27 -0700

    moved everything to the right places

--- announcements/views.py
+++ announcements/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-
+from announcements.models import .
 
 def index(request):
     return HttpResponse("Hello, world. You're at the announcements index.")
+
+
+
+def atext(request, announcement_id):
+    try
+        text = Announcement.announce_text
+        context = {'announcement_id': announcement_id}
+    except Announcement.DoesNotExist:
+        raise Http404("Announcement does not exist")
+    return render(request,'chirps/atext.html', context)
