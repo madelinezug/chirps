c1ff2604a69a5585e99fa9d7225824da78cddc7b
Maddie Zug <maddiezug@Maddies-MacBook-Pro.local>
2018-03-26 21:01:13 -0700

    added comma

--- chirps/settings.py
+++ chirps/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'chirps',
-    'announcements'
+    'announcements',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
