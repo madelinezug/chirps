0b07981e15ff3b6e51d30261f2bf8d50a9f33026
Mark Penrod <mark.penrod@pomona.edu>
2018-04-01 23:01:41 -0700

    working on frontend/backend communication

---
+++ announcements/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+from .models import Announcement
+
+class SubmitAnnounceForm(forms.ModelForm):
+
+    class Meta:
+        model = Announcement
+        fields = ['announce_text','announce_ID']
